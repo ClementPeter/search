895dbaaa8789d752142a342051b6376b8dbb910e
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2022-12-06 22:07:46 +0100

    ✨ added: model data for search

---
+++ lib/models/data.dart
@@ -0,0 +1,21 @@
+class Month {
+  String? month;
+  int? number;
+
+  Month({this.month, this.number});
+}
+
+List<Month> yearMonth = [
+  Month(month: "January", number: 1),
+  Month(month: "Feburary", number: 2),
+  Month(month: "March", number: 3),
+  Month(month: "April", number: 4),
+  Month(month: "May", number: 5),
+  Month(month: "June", number: 6),
+  Month(month: "July", number: 7),
+  Month(month: "August", number: 8),
+  Month(month: "September", number: 9),
+  Month(month: "October", number: 10),
+  Month(month: "November", number: 11),
+  Month(month: "December", number: 12),
+];
