04693074c8ba214d4dc6b9c792cbedc363389dce
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2022-12-07 10:49:12 +0100

    ✨ Search functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:search/models/data.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Search Month Demo Home Page'),
+      home: const MyHomePage(title: 'Search Month'),
     );
   }
 }
@@ -22,46 +23,84 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  final String title; 
+  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  TextEditingController controller = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  int?
+      dataLength; //variable to the text.lenght of the searched item in the TextFormField
 
+  List<Data>?
+      data; //variable that stores the full content search item after the user has searh
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
+          centerTitle: true,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        //Contains our TextFormfield for search
+        body: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: controller,
+                onChanged: ((value) {
+                //  print(":::::::::::::::::::onchanged $value");
+                  setState(() {
+                    //Get the lenght of the "value" typed
+                    dataLength = socialData
+                        .where((element) =>
+                            element.title!.toLowerCase().startsWith(value))
+                        .length;
+                    print(":::::::::::::::::::onchanged $dataLength");
+
+                    //get the searched value 
+                    data = socialData
+                        .where((element) =>
+                            element.title!.toLowerCase().startsWith(value))
+                        .toList();
+                    print(":::::::::::::::::::onchanged $data");
+                  });
+                }),
+                // onEditingComplete: (() {
+                //   print(
+                //       ":::::::::::::::::::onEditingcomplete ${controller.text}");
+                // }),
+                decoration: InputDecoration(
+                  hintText: "Search names",
+                  suffixIcon: GestureDetector(
+                    onTap: () {},
+                    child: const  Icon(
+                      Icons.search,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            ...List.generate(socialData.length, (index) {
+              final snapshot = socialData[index];
+
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
+                  shape: RoundedRectangleBorder(
+                    // side: BorderSide(width: 2, color: Colors.amberAccent),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  tileColor: Color.fromARGB(255, 201, 198, 198),
+                  title: Text(snapshot.title!),
+                  subtitle: Text(snapshot.number!.toString()),
+                ),
+              );
+            })
+          ],
+        ));
   }
 }
--- lib/models/data.dart
+++ lib/models/data.dart
@@ -1,21 +1,24 @@
-class Month {
-  String? month;
+class Data {
+  String? title;
   int? number;
 
-  Month({this.month, this.number});
+  Data({this.title, this.number});
 }
 
-List<Month> yearMonth = [
-  Month(month: "January", number: 1),
-  Month(month: "Feburary", number: 2),
-  Month(month: "March", number: 3),
-  Month(month: "April", number: 4),
-  Month(month: "May", number: 5),
-  Month(month: "June", number: 6),
-  Month(month: "July", number: 7),
-  Month(month: "August", number: 8),
-  Month(month: "September", number: 9),
-  Month(month: "October", number: 10),
-  Month(month: "November", number: 11),
-  Month(month: "December", number: 12),
+//List of Data we arew going to be searching from
+List<Data> socialData = [
+  Data(title: "David", number: 200),
+  Data(title: "Mark", number: 302),
+  Data(title: "John", number: 123),
+  Data(title: "Gabriel", number: 445),
+  Data(title: "Peter", number: 524),
+  Data(title: "Daniel", number: 613),
+  Data(title: "Samson", number: 137),
+  Data(title: "Love", number: 328),
+  Data(title: "Thomas", number: 429),
+  Data(title: "Jeremiah", number: 410),
+  Data(title: "Micheal", number: 112),
+  Data(title: "Matthew", number: 141),
+  Data(title: "Joshua", number: 126),
+  Data(title: "Joseph", number: 126),
 ];
