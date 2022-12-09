0090ac766175886fc34245a28edeb11e8742b99e
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2022-12-09 14:00:19 +0100

    -💡 Demo : Search user

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Search Month'),
+      home: const MyHomePage(title: 'Search Names'),
     );
   }
 }
@@ -32,75 +32,72 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   TextEditingController controller = TextEditingController();
 
-  int?
-      dataLength; //variable to the text.lenght of the searched item in the TextFormField
+  //variable to the text.lenght of the searched item in the TextFormField
+  int? dataLength;
 
-  List<Data>?
-      data; //variable that stores the full content search item after the user has searh
+  //variable that stores the full content search item after the user has searh
+  List<Data>? data;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-          centerTitle: true,
-        ),
-        //Contains our TextFormfield for search
-        body: ListView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                controller: controller,
-                onChanged: ((value) {
-                //  print(":::::::::::::::::::onchanged $value");
-                  setState(() {
-                    //Get the lenght of the "value" typed
-                    dataLength = socialData
-                        .where((element) =>
-                            element.title!.toLowerCase().startsWith(value))
-                        .length;
-                    print(":::::::::::::::::::onchanged $dataLength");
-
-                    //get the searched value 
-                    data = socialData
-                        .where((element) =>
-                            element.title!.toLowerCase().startsWith(value))
-                        .toList();
-                    print(":::::::::::::::::::onchanged $data");
-                  });
-                }),
-                // onEditingComplete: (() {
-                //   print(
-                //       ":::::::::::::::::::onEditingcomplete ${controller.text}");
-                // }),
-                decoration: InputDecoration(
-                  hintText: "Search names",
-                  suffixIcon: GestureDetector(
-                    onTap: () {},
-                    child: const  Icon(
-                      Icons.search,
-                    ),
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      //Contains our TextFormfield for search
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              onChanged: ((value) {
+                //print(":::::::::::::::::::onchanged value $value");
+                setState(() {
+                  //Get the lenght of the "value" typed
+                  dataLength = socialData
+                      .where((element) =>
+                          element.title!.toLowerCase().startsWith(value))
+                      .length;
+                  //print(":::::::::::::::::::onchanged dataLength $dataLength");
+                  //get the searched value with the element property in the List [SocialData]
+                  data = socialData
+                      .where((element) =>
+                          element.title!.toLowerCase().startsWith(value))
+                      .toList();
+                  //print(":::::::::::::::::::onchanged data $data");
+                });
+              }),
+              decoration: InputDecoration(
+                hintText: "Search names",
+                suffixIcon: GestureDetector(
+                  onTap: () {},
+                  child: const Icon(
+                    Icons.search,
                   ),
                 ),
               ),
             ),
-            ...List.generate(socialData.length, (index) {
-              final snapshot = socialData[index];
-
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ListTile(
-                  shape: RoundedRectangleBorder(
-                    // side: BorderSide(width: 2, color: Colors.amberAccent),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  tileColor: Color.fromARGB(255, 201, 198, 198),
-                  title: Text(snapshot.title!),
-                  subtitle: Text(snapshot.number!.toString()),
+          ),
+          //Generated stuff that comes as the search result
+          //for index : dataLength ?? socialData.length,---works too
+          ...List.generate(dataLength == null ? socialData.length : dataLength!,
+              (index) {
+            // final snapshot = socialData[index];
+            final snapshot = data == null ? socialData[index] : data![index];
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
                 ),
-              );
-            })
-          ],
-        ));
+                tileColor: const Color.fromARGB(255, 201, 198, 198),
+                title: Text(snapshot.title!),
+                subtitle: Text(snapshot.number!.toString()),
+              ),
+            );
+          })
+        ],
+      ),
+    );
   }
 }
